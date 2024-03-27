8180cc2aa8a4b6b262329b8760c3d632021ebd9a
Veilind Maynius <veilindmaynius04@gmail.com>
2024-03-27 19:27:39 +0700

    add welcome page

--- better_u/lib/main.dart
+++ better_u/lib/main.dart
@@ -16,7 +16,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(useMaterial3: false),
-      home: OnbordingPage()
+      home: OnbordingPage(),
+      debugShowCheckedModeBanner: false,      
     );
   }
 }
--- better_u/lib/screens/welcome.dart
+++ better_u/lib/screens/welcome.dart
@@ -18,19 +18,19 @@ class OnbordingPage extends StatelessWidget {
         PageViewModel(
           title: 'Build Your Health',
           body: 'Starting a healthy life? Why Not!',
-          image: Image.asset('/assets/intro-pic1.png', width: 350),
-          decoration: pageDecoration
+          image: Image.asset('assets/intro-pic1.png', width: 350),
+          decoration: pageDecoration,
         ),
         PageViewModel(
           title: 'Transform Your Body',
           body: 'Feel the difference, be your BEST SELF!',
-          image: Image.asset('/assets/intro-pic2.png', width: 350),
+          image: Image.asset('assets/intro-pic2.png', width: 350),
           decoration: pageDecoration
         ),
         PageViewModel(
           title: 'Get Started Now!',
           body: 'Train with us, Reach your GOALS',
-          image: Image.asset('/assets/intro-pic3.png', width: 350),
+          image: Image.asset('assets/intro-pic3.png', width: 350),
           decoration: pageDecoration,
           footer: ElevatedButton(
             onPressed: () {},
