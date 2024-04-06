98d0505411b4863ebd4978200b5d71152e0b1289
Veilind Maynius <veilindmaynius04@gmail.com>
2024-04-06 21:04:27 +0700

    adding carousel with dots indicator

--- better_u/lib/screens/home.dart
+++ better_u/lib/screens/home.dart
@@ -1,45 +1,62 @@
+import 'package:better_u/screens/models/carousel.dart';
+import 'package:better_u/screens/models/carousel_indicator.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  int _currentIndex = 0;
+  List<String> imagePaths = [
+    'images/carousel1.png',
+    'images/carousel2.png',
+    'images/carousel3.jpg',
+  ];
+
   @override
   Widget build(BuildContext context) {
     double screenWidth = MediaQuery.of(context).size.width;
 
     return Scaffold(
       appBar: AppBar(
-        title: Row(
-          children: [
-            Container(
-              decoration: BoxDecoration(
-                shape: BoxShape.circle,
-                border: Border.all(
-                  color: const Color.fromARGB(255, 226, 189, 255),
-                  width: 2, // Adjust the width of the outline as needed
+        title: Padding(
+          padding: EdgeInsets.only(top: 8.0),
+          child :
+            Row(
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    border: Border.all(
+                      color: const Color.fromARGB(255, 226, 189, 255),
+                      width: 2, // Adjust the width of the outline as needed
+                    ),
+                  ),
+                  child: CircleAvatar(
+                    radius: 20,
+                    backgroundColor: Colors.grey[300],
+                    // backgroundImage: AssetImage('images/profile-picture.jpg'), 
+                  ),
                 ),
-              ),
-              child: CircleAvatar(
-                radius: 20,
-                backgroundColor: Colors.grey[300],
-                // backgroundImage: AssetImage('images/profile-picture.jpg'), 
-              ),
+                const SizedBox(width: 8),
+                const Text('Hi, Kelly!', style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
+              ],
             ),
-            const SizedBox(width: 8),
-            const Text('Hi, Kelly!', style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
-          ],
         ),
       ),
       body: Padding(
         padding: const EdgeInsets.only(top: 16),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             CarouselSlider(
-              items: [
-                ContainImage(imagePath: 'images/carousel1.png'),
-                ContainImage(imagePath: 'images/carousel2.png'),
-                ContainImage(imagePath: 'images/carousel3.jpg'),
-              ], 
+             items: imagePaths.map((imagePath) {
+                return ContainImage(imagePath: imagePath);
+              }).toList(), 
               options: CarouselOptions(
                 height: 200,
                 aspectRatio: screenWidth / 200,
@@ -52,31 +69,28 @@ class Home extends StatelessWidget {
                 autoPlayCurve: Curves.fastOutSlowIn,
                 enlargeCenterPage: true,
                 scrollDirection: Axis.horizontal,
+                onPageChanged: (index, reason) {
+                  setState(() {
+                    _currentIndex = index; // Update current index when page changes
+                  });
+                },
               ),
             ),
+            SizedBox(height: 10), 
+            CarouselIndicator(
+              itemCount: 3,
+              currentIndex: _currentIndex,
+              indicatorSize: 8.0,
+              spacing: 5.0,
+            ),
+            SizedBox(height: 10),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text('Top Picks', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),),
+            )
           ],
         ),
       )
-    );
-  }
-}
-
-class ContainImage extends StatelessWidget {
-  final String imagePath;
-
-  const ContainImage({required this.imagePath});
-
-  @override
-  Widget build(BuildContext context) {
-    return LayoutBuilder(
-      builder: (context, constraints) {
-        return Image.asset(
-          imagePath,
-          width: constraints.maxWidth,
-          height: constraints.maxHeight,
-          fit: BoxFit.cover,
-        );
-      },
     );
   }
 }
