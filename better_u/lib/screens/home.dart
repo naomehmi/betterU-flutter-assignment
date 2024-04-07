37787da5b4b0b87a71a9108aba34b916367fa002
221111798-naomi <221111798@students.mikroskil.ac.id>
2024-04-07 13:15:25 +0700

    added top picks on home page

--- better_u/lib/screens/home.dart
+++ better_u/lib/screens/home.dart
@@ -1,10 +1,13 @@
 import 'package:better_u/screens/models/carousel.dart';
 import 'package:better_u/screens/models/carousel_indicator.dart';
+import 'package:better_u/screens/models/top_picks_card.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 
 class Home extends StatefulWidget {
+  const Home({super.key});
+
   @override
   _HomeState createState() => _HomeState();
 }
@@ -17,6 +20,29 @@ class _HomeState extends State<Home> {
     'images/carousel3.jpg',
   ];
 
+  List<Map<String,dynamic>> topPicks = [
+    {"coverImage" : "images/top-picks-sample1.jpg",
+    "days" : 26,
+    "interval" : "30-45",
+    "title" : "2024 Weight Loss Challenge"
+    },
+    {"coverImage" : "images/top-picks-sample2.jpg",
+    "days" : 30,
+    "interval" : "20-40",
+    "title" : "Beginner Abs Challenge"
+    },
+    {"coverImage" : "images/top-picks-sample3.jpg",
+    "days" : 14,
+    "interval" : "35-45",
+    "title" : "Flat Belly Challenge"
+    },
+    {"coverImage" : "images/top-picks-sample4.jpg",
+    "days" : 21,
+    "interval" : "15-30",
+    "title" : "Strong Arms Challenge"
+    },
+  ];
+
   @override
   Widget build(BuildContext context) {
     double screenWidth = MediaQuery.of(context).size.width;
@@ -24,7 +50,7 @@ class _HomeState extends State<Home> {
     return Scaffold(
       appBar: AppBar(
         title: Padding(
-          padding: EdgeInsets.only(top: 8.0),
+          padding: const EdgeInsets.only(top: 8.0),
           child :
             Row(
               children: [
@@ -36,10 +62,10 @@ class _HomeState extends State<Home> {
                       width: 2, // Adjust the width of the outline as needed
                     ),
                   ),
-                  child: CircleAvatar(
+                  child: const CircleAvatar(
                     radius: 20,
-                    backgroundColor: Colors.grey[300],
-                    // backgroundImage: AssetImage('images/profile-picture.jpg'), 
+                    //backgroundColor: Colors.grey[300],
+                    backgroundImage: AssetImage('images/profile-picture.jpg'), 
                   ),
                 ),
                 const SizedBox(width: 8),
@@ -48,47 +74,64 @@ class _HomeState extends State<Home> {
             ),
         ),
       ),
-      body: Padding(
-        padding: const EdgeInsets.only(top: 16),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            CarouselSlider(
-             items: imagePaths.map((imagePath) {
-                return ContainImage(imagePath: imagePath);
-              }).toList(), 
-              options: CarouselOptions(
-                height: 200,
-                aspectRatio: screenWidth / 200,
-                viewportFraction: 1.0,
-                initialPage: 0,
-                enableInfiniteScroll: true,
-                autoPlay: true,
-                autoPlayInterval: Duration(seconds: 3),
-                autoPlayAnimationDuration: Duration(milliseconds: 800),
-                autoPlayCurve: Curves.fastOutSlowIn,
-                enlargeCenterPage: true,
-                scrollDirection: Axis.horizontal,
-                onPageChanged: (index, reason) {
-                  setState(() {
-                    _currentIndex = index; // Update current index when page changes
-                  });
-                },
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.only(top: 16),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              CarouselSlider(
+               items: imagePaths.map((imagePath) {
+                  return ContainImage(imagePath: imagePath);
+                }).toList(), 
+                options: CarouselOptions(
+                  height: 200,
+                  aspectRatio: screenWidth / 200,
+                  viewportFraction: 1.0,
+                  initialPage: 0,
+                  enableInfiniteScroll: true,
+                  autoPlay: true,
+                  autoPlayInterval: const Duration(seconds: 3),
+                  autoPlayAnimationDuration: const Duration(milliseconds: 800),
+                  autoPlayCurve: Curves.fastOutSlowIn,
+                  enlargeCenterPage: true,
+                  scrollDirection: Axis.horizontal,
+                  onPageChanged: (index, reason) {
+                    setState(() {
+                      _currentIndex = index; // Update current index when page changes
+                    });
+                  },
+                ),
               ),
-            ),
-            SizedBox(height: 10), 
-            CarouselIndicator(
-              itemCount: 3,
-              currentIndex: _currentIndex,
-              indicatorSize: 8.0,
-              spacing: 5.0,
-            ),
-            SizedBox(height: 10),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Text('Top Picks', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),),
-            )
-          ],
+              const SizedBox(height: 10), 
+              CarouselIndicator(
+                itemCount: 3,
+                currentIndex: _currentIndex,
+                indicatorSize: 8.0,
+                spacing: 5.0,
+              ),
+              const SizedBox(height: 10),
+              const Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Text('Top Picks', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),),
+              ),
+              CarouselSlider(
+                items: topPicks.map((tp) {
+                  return TopPicksCard(
+                    coverImage: tp["coverImage"],
+                    days: tp["days"],
+                    interval: tp["interval"],
+                    title: tp["title"],
+                  );
+                }).toList(),
+                options: CarouselOptions(
+                  height: 300,
+                  viewportFraction: 0.45,
+                  enableInfiniteScroll: false,
+                  initialPage: 1
+                ))
+            ],
+          ),
         ),
       )
     );
